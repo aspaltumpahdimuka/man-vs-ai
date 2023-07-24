1312164a0581b6a616babe482f870776726a4924
Agus <bountygus@gmail.com>
2023-07-24 10:35:41 +0700

    script jankenpon

---
+++ script.js
@@ -0,0 +1,59 @@
+const JANKENPON = ["batu", "gunting", "kertas"]
+let playerHP = 10
+let AiHP = 10
+
+const getAIChoice = () => {
+    const randomIndex = Math.floor(Math.random() * JANKENPON.length)
+    return JANKENPON[randomIndex]
+}
+
+
+const getPlayerChoice = (num) => {
+    return JANKENPON[num]
+}
+
+const isPlayerWon = (playerChoice, aiChoice) => {
+    const isTie = aiChoice === playerChoice
+    const AIWonCondition = 
+        aiChoice === "batu" && playerChoice === "gunting" || 
+        aiChoice === "gunting" && playerChoice === "kertas" || 
+        aiChoice === "kertas" && playerChoice === "batu"
+    const playerWonCondition = 
+        playerChoice === "batu" && aiChoice === "gunting" || 
+        playerChoice === "gunting" && aiChoice === "kertas" || 
+        playerChoice === "kertas" && aiChoice === "batu"
+
+    if (isTie) {
+        return "Seri"
+    } else if (AIWonCondition) {
+        return false
+    } else if (playerWonCondition) {
+        return true
+    }
+}
+
+const playGame = () => {
+    let aiChoice = getAIChoice()
+    let playerChoice = parseInt(prompt("Pilih 0 - 2: "))
+
+    isPlayerWon(playerChoice, aiChoice)
+
+    if (isPlayerWon) {
+        AiHP--
+    } else if (!isPlayerWon) {
+        playerHP--
+    } else {
+        return "Seri"
+    }
+}
+
+while (playerHP > 0 && AiHP > 0) {
+    playGame()
+}
+
+if (AiHP === 0) {
+    console.log("Game Over! Dunia berhasil diselamatkan!")
+}
+if (playerHP === 0) {
+    console.log("Game Over! Dunia dikuasai AI!")
+}
